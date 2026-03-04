1b8dcf3bbc0a795a64c9d1a78385b83745d97eb9
thedonmusa <126988717+thedonmusa@users.noreply.github.com>
2026-03-04 15:29:19 +0200

    Delete app/app/app/sign-in/[[...sign-in]]/app/sign-up/[[...sign-up]]/app/dashboard/app/sign-in/[[...rest]]/page.tsx

--- app/app/app/sign-in/[[...sign-in]]/app/sign-up/[[...sign-up]]/app/dashboard/app/sign-in/[[...rest]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function SignInPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-950">
-      <SignIn />
-    </main>
-  );
-}
